5fb07da8c5ad076f1c08a3a45a29761b9d87fb94
Parveen Verma <vparveen012@gmail.com>
2025-11-28 11:24:25 +0530

    js

---
+++ index.js
@@ -0,0 +1,16 @@
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+let a=19;
+const b=20;
+var c=21;
+function SUM(){
+    let b=30;
+    function SUB(){
+        console.log(b);
+    }}
+
+    SUM();
+    SUB();
